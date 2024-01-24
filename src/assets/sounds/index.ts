3a64412fe6ac8de667640e69c76a0cace4eab509
ibrahim Tahayna <ibrahim.tahainy@gmail.com>
2024-01-24 23:03:11 +0200

    add sound for animals

--- src/assets/sounds/index.ts
+++ src/assets/sounds/index.ts
@@ -1,1 +1,8 @@
 export { default as Bird } from "./bird.mp3";
+export { default as Cat } from "./cat.mp3";
+export { default as Chicken } from "./chicken.mp3";
+export { default as Dog } from "./dog.mp3";
+export { default as Elephant } from "./elephant.mp3";
+export { default as Giraffe } from "./giraffe.mp3";
+export { default as Horse } from "./horse.mp3";
+export { default as Monkey } from "./monkey.mp3";
